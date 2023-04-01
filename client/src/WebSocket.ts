fef828b1c23b5d7e70e1949371241bd622c8c925
Philipp Khromov <filooloog@mail.ru>
2023-04-01 18:00:47 +0300

    feat: add WebSocket stub

---
+++ client/src/WebSocket.ts
@@ -0,0 +1,44 @@
+import { WSEvent } from './Config/Enums';
+import MessageEvent from './MessageEvent';
+import Server from './Server';
+import TMessageEvent from './Types/MessageEvent';
+
+export default class WebSocket extends EventTarget {
+  private _url: string;
+  private _protocol: string;
+  private _server: Server;
+
+  constructor(url: string, protocol?: string) {
+    super();
+    this._server = new Server(this);
+    this._url = url;
+    this._protocol = protocol || 'ws';
+  }
+
+  onopen = (cb: () => void) => {
+    this._server.addEventListener(WSEvent.open, cb);
+  };
+
+  onclose = (cb: () => void) => {
+    this._server.addEventListener(WSEvent.close, cb);
+  };
+
+  onmessage = (cb: (event: TMessageEvent) => void) => {
+    this._server.addEventListener(WSEvent.message, (e) => {
+      if (e.hasOwnProperty('data')) {
+        cb(e as TMessageEvent);
+      }
+    });
+  };
+  onerror = (cb: () => void) => {
+    this._server.addEventListener(WSEvent.error, cb);
+  };
+
+  close = () => {
+    this.dispatchEvent(new Event(WSEvent.close));
+  };
+
+  send = (data: string) => {
+    this.dispatchEvent(new MessageEvent<string>(WSEvent.message, data));
+  };
+}
