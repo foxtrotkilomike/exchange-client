29768f2b6f0c68503bf3c86be67a38d80f308820
Philipp Khromov <filooloog@mail.ru>
2023-04-01 18:02:15 +0300

    feat: change WSClient connection methods signature

--- client/src/WSClient.ts
+++ client/src/WSClient.ts
@@ -6,8 +6,9 @@ import {
   OrderSide,
   ServerMessageType,
 } from './Config/Enums';
-import { ClientMessage } from './Models/ClientMessages';
+import { ClientEnvelope } from './Models/ClientMessages';
 import { ServerEnvelope } from './Models/ServerMessages';
+import WebSocket from './WebSocket';
 
 export default class WSConnector {
   connection: WebSocket | undefined;
@@ -18,34 +19,39 @@ export default class WSConnector {
 
   connect = () => {
     this.connection = new WebSocket('ws://127.0.0.1:3000/ws/');
-    this.connection.onclose = () => {
+    this.connection.onclose(() => {
+      console.log('Server closed the connection');
       this.connection = undefined;
-    };
+    });
 
-    this.connection.onerror = () => {};
+    this.connection.onerror(() => {});
 
-    this.connection.onopen = () => {};
+    this.connection.onopen(() => {
+      console.log('Server opened the connection');
+    });
 
-    this.connection.onmessage = (event) => {
+    this.connection.onmessage((event) => {
       const message: ServerEnvelope = JSON.parse(event.data);
       switch (message.messageType) {
         case ServerMessageType.success:
+          console.log('Client received success', message.message);
           break;
         case ServerMessageType.error:
           break;
         case ServerMessageType.executionReport:
+          console.log('Client received executionReport', message.message);
           break;
         case ServerMessageType.marketDataUpdate:
           break;
       }
-    };
+    });
   };
 
   disconnect = () => {
     this.connection?.close();
   };
 
-  send = (message: ClientMessage) => {
+  send = (message: ClientEnvelope) => {
     this.connection?.send(JSON.stringify(message));
   };
 
