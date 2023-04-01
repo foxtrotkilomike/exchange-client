98d2b10b7b30c4e38044a3fc65366d74a4e510be
Philipp Khromov <filooloog@mail.ru>
2023-04-01 17:57:00 +0300

    refactor: Enums internal description

--- client/src/Config/Enums.ts
+++ client/src/Config/Enums.ts
@@ -5,10 +5,10 @@ export enum ClientMessageType {
 }
 
 export enum ServerMessageType {
-  success = 1,
-  error,
-  executionReport,
-  marketDataUpdate,
+  success = 'success',
+  error = 'error',
+  executionReport = 'executionReport',
+  marketDataUpdate = 'marketDataUpdate',
 }
 
 export enum OrderSide {
@@ -27,4 +27,11 @@ export enum Instrument {
   eur_usd = 'EUR/USD',
   eur_rub = 'EUR/RUB',
   usd_rub = 'USD/RUB',
+}
+
+export enum WSEvent {
+  close = 'close',
+  error = 'error',
+  message = 'message',
+  open = 'open',
 }
