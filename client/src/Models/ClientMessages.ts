e8e30fb63e9bfa7bc5c364b1ea1a1d2754aaffdd
Philipp Khromov <filooloog@mail.ru>
2023-04-01 17:58:49 +0300

    refactor: ClientMessages

--- client/src/Models/ClientMessages.ts
+++ client/src/Models/ClientMessages.ts
@@ -5,21 +5,27 @@ import { ClientMessageType, Instrument, OrderSide } from '../Config/Enums';
 
 export interface ClientEnvelope extends Envelope {
   messageType: ClientMessageType;
+  message: ClientMessage;
 }
 
-export type ClientMessage = Message;
+export type ClientMessageInit = Message;
 
-export interface SubscribeMarketData extends ClientMessage {
+export interface SubscribeMarketData extends ClientMessageInit {
   instrument: Instrument;
 }
 
-export interface UnsubscribeMarketData extends ClientMessage {
+export interface UnsubscribeMarketData extends ClientMessageInit {
   subscriptionId: string;
 }
 
-export interface PlaceOrder extends ClientMessage {
+export interface PlaceOrder extends ClientMessageInit {
   instrument: Instrument;
   side: OrderSide;
   amount: Decimal;
   price: Decimal;
 }
+
+export type ClientMessage =
+  | SubscribeMarketData
+  | UnsubscribeMarketData
+  | PlaceOrder;
